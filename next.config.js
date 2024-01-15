410fabab3b80b3699246ac1da4db5ab875d2c0d1
G. H. Krishna Chaitanya <g.h.krishnachaitanya@Gs-Mac-mini.local>
2024-01-15 23:16:00 +0530

    The homepage functions as expected

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        domains:['cdn.hashnode.com', 'res.cloudinary.com']
+    }
+}
 
 module.exports = nextConfig
