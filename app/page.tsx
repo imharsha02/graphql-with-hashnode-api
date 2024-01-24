c49c018a4fadafa95cdc5073cac8d5a4fb5b176e
Harshavardhana Krishna Chaitanya <kccodes365@gmail.com>
2024-01-24 19:15:51 +0530

    Styling the home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,25 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+  Merriweather_Sans,
+  Roboto_Condensed,
+  Open_Sans,
+} from "next/font/google";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { useDetailsContext } from "./context/DetailsContext";
+const merriweather_sans = Merriweather_Sans({ subsets: ["latin"] });
+const open_sans = Open_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+});
+const roboto = Roboto_Condensed({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 export default function Home() {
   const [searchingUser, setSearchingUser] = useState<string>("");
 
@@ -27,14 +34,16 @@ export default function Home() {
 
   return (
     <main className="min-h-screen space-y-2">
-      <h1 className="font-bold text-3xl mb-2">HashProfiles</h1>
+      <h1 className={` mb-2 ${merriweather_sans.className} text-3xl`}>
+        HashProfiles
+      </h1>
 
       {/* Search bar */}
       <div className="flex items-center gap-2">
         <Input
           type="text"
           placeholder="Enter a username..."
-          className="w-max"
+          className="w-max ${open_sans.className}"
           value={searchingUser}
           onChange={(e) => {
             setSearchingUser(e.target.value);
@@ -56,30 +65,46 @@ export default function Home() {
         </Button>
       </div>
       {/* Options between users */}
-      <p>OR Select a hashnode user from below</p>
+      <p className="pt-5 ${open_sans.className}">
+        OR Select a hashnode user from below
+      </p>
 
-      <div className="flex space-x-2">
+      <div className="flex space-x-2 pt-5">
         {/* USER 1 */}
         <Link href="/victoria">
-          
           <Card>
-            <CardHeader>
-              <CardTitle>victoria</CardTitle>
-            </CardHeader>
-            <CardContent>
+            <CardContent className="pt-6 space-y-5">
               <div className="flex items-center gap-2">
-                <span>Username:</span> <p>victoria</p>
+                <span
+                  className={`tracking-wide text-xl ${open_sans.className} font-bold`}
+                >
+                  Username:
+                </span>{" "}
+                <p className={`tracking-wide text-lg ${open_sans.className}`}>
+                  victoria
+                </p>
               </div>
               <div className="flex items-center gap-2">
-                <span>Name:</span> <p>Victoria Lo</p>
+                <span
+                  className={`tracking-wide text-xl ${open_sans.className} font-bold`}
+                >
+                  Name:
+                </span>{" "}
+                <p className={` text-lg tracking-wide ${open_sans.className}`}>
+                  Victoria Lo
+                </p>
               </div>
               <div className="flex items-center gap-2">
-                <span>Profile pic:</span>
+                <span
+                  className={`tracking-wide text-xl ${open_sans.className} font-bold`}
+                >
+                  Profile pic:
+                </span>
                 <Image
                   src="https://cdn.hashnode.com/res/hashnode/image/upload/v1672984921463/25B4G5fCJ.jpg?w=500&h=500&fit=crop&crop=faces&auto=compress,format&format=webp"
                   alt="profile pic"
-                  width={32}
-                  height={32}
+                  width={45}
+                  height={45}
                   className="rounded-lg"
                 />
               </div>
@@ -89,39 +114,39 @@ export default function Home() {
 
         {/* USER 2 */}
         <Link href="/codewithbhargav">
-          {/* <div className="border border-black w-max rounded-md p-2">
-            codewithbhargav
-            <p>Username: codewithbhargav</p>
-            <p>Name: Bhargav Ponnapalli</p>
-            <div className="flex items-center gap-2">
-              <p>Profile pic:</p>
-              <Image
-                src="https://cdn.hashnode.com/res/hashnode/image/upload/v1647778136262/PTwDclbQa.png?w=240&h=240&fit=crop&crop=faces&auto=compress,format&format=webp"
-                alt="profile pic"
-                width={32}
-                height={32}
-                className="rounded-lg"
-              />
-            </div>
-          </div> */}
           <Card>
-            <CardHeader>
-              <CardTitle>codewithbhargav</CardTitle>
-            </CardHeader>
-            <CardContent>
+            <CardContent className="pt-6 space-y-5">
               <div className="flex items-center gap-2">
-                <span>Username:</span> <p>codewithbhargav</p>
+                <span
+                  className={`tracking-wide text-xl ${open_sans.className} font-bold`}
+                >
+                  Username:
+                </span>{" "}
+                <p className={`tracking-wide ${open_sans.className} text-lg`}>
+                  codewithbhargav
+                </p>
               </div>
               <div className="flex items-center gap-2">
-                <span>Name:</span> <p>Bhargav Ponnapalli</p>
+                <span
+                  className={`tracking-wide text-xl ${open_sans.className} font-bold`}
+                >
+                  Name:
+                </span>{" "}
+                <p className={`tracking-wide text-lg ${open_sans.className}`}>
+                  Bhargav Ponnapalli
+                </p>
               </div>
               <div className="flex items-center gap-2">
-                <span>Profile pic:</span>
+                <span
+                  className={`tracking-wide text-xl ${open_sans.className} font-bold`}
+                >
+                  Profile pic:
+                </span>
                 <Image
                   src="https://cdn.hashnode.com/res/hashnode/image/upload/v1647778136262/PTwDclbQa.png?w=240&h=240&fit=crop&crop=faces&auto=compress,format&format=webp"
                   alt="profile pic"
-                  width={32}
-                  height={32}
+                  width={45}
+                  height={45}
                   className="rounded-lg"
                 />
               </div>
@@ -132,66 +157,88 @@ export default function Home() {
 
       {/* Recent searches */}
       <div className="mt-2">
-        <h2 className="font-bold text-2xl">Recent Searches</h2>
+        <h2 className={`pt-5 text-2xl tracking-wider ${roboto.className}`}>
+          Recent Searches
+        </h2>
         {recentSearches.map((recentSearch, index) => (
-          <button
-            key={index}
-            onClick={() => {
-              setSearchingUser(recentSearch);
-              setSearchedUser(recentSearch);
-            }}
-            className="flex space-y-3 text-lg hover:underline"
-          >
+          <p key={index} className="flex space-y-3 text-lg pt-2">
             {recentSearch}
-          </button>
+          </p>
         ))}
       </div>
 
       {/* Rendering details */}
       {searchedUser && details && details.data.user ? (
         // USER DETAILS CONTAINER
-        <div className="mx-auto flex flex-col max-w-2xl space-y-3">
+        <Card className="mx-auto flex flex-col max-w-2xl space-y-3">
           {/* SECTION TITLE */}
-          <h2 className="font-bold text-2xl">User details</h2>
+          <CardHeader>
+            <CardTitle
+              className={`font-bold text-2xl tracking-wider ${roboto.className}`}
+            >
+              User details
+            </CardTitle>
+          </CardHeader>
 
-          {/* USERNAME */}
-          <div className="flex items-center gap-1">
-            <span className="font-semibold text-xl">Username:</span>{" "}
-            <p className="text-lg">{details.data.user.username}</p>
-          </div>
+          <CardContent className="space-y-5">
+            {/* USERNAME */}
+            <div className="flex gap-1">
+              <span
+                className={` ${open_sans.className} tracking-wide font-semibold text-xl`}
+              >
+                Username:
+              </span>{" "}
+              <p className={` ${open_sans.className} tracking-wide text-lg`}>
+                {details.data.user.username}
+              </p>
+            </div>
 
-          {/* NAME */}
-          <div className="flex items-center gap-1">
-            <span className="font-semibold text-xl">Name:</span>{" "}
-            <p className="text-lg">{details.data.user.name}</p>
-          </div>
+            {/* NAME */}
+            <div className="flex gap-1">
+              <span
+                className={` ${open_sans.className} tracking-wide font-semibold text-xl`}
+              >
+                Name:
+              </span>{" "}
+              <p className={` ${open_sans.className} tracking-wide text-lg`}>
+                {details.data.user.name}
+              </p>
+            </div>
 
-          {/* PROFILE PICTURE */}
-          <div className="flex items-center gap-1">
-            <span className="font-semibold text-xl">Profile pic:</span>
-            <Image
-              src={details.data.user.profilePicture}
-              alt="profile picture"
-              width={50}
-              height={50}
-              className="rounded-lg"
-            />
-          </div>
+            {/* PROFILE PICTURE */}
+            <div className="flex gap-1">
+              <span
+                className={` ${open_sans.className} tracking-wide font-semibold text-xl`}
+              >
+                Profile pic:
+              </span>
+              <Image
+                src={details.data.user.profilePicture}
+                alt="profile picture"
+                width={50}
+                height={50}
+                className="rounded-lg"
+              />
+            </div>
 
-          {/* USER BIO */}
-          <div className="flex items-center gap-1">
-            <span className="font-semibold text-xl">Bio:</span>{" "}
-            <p className="text-lg">{details.data.user.bio.text}</p>
-          </div>
+            {/* USER BIO */}
+            <div className="flex gap-1">
+              <span
+                className={` ${open_sans.className} tracking-wide font-semibold text-xl`}
+              >
+                Bio:
+              </span>{" "}
+              <p className={` ${open_sans.className} tracking-wide text-lg`}>
+                {details.data.user.bio.text}
+              </p>
+            </div>
 
-          {/* LINK TO USER PAGE */}
-          <Link
-            href={`/${searchedUser}`}
-            className="py-2 px-5 bg-white w-max hover:scale-110 transition"
-          >
-            Know more
-          </Link>
-        </div>
+            {/* LINK TO USER PAGE */}
+            <Button asChild>
+              <Link href={`${searchedUser}`}>Know More</Link>
+            </Button>
+          </CardContent>
+        </Card>
       ) : searchedUser ? (
         <h1>No user with that name</h1>
       ) : null}
